d9a468bfed314d58764df57c9cf9ce45215ea787
BowTiedGnome <119234633+bowtiedgnome@users.noreply.github.com>
2024-06-09 00:31:24 -0500

    work: refactor to a base class. add typed get/default methods to convert value to types

--- env/deno/mod.ts
+++ env/deno/mod.ts
@@ -1,138 +1,9 @@
-import { expand } from "../expand.ts";
-import type { Env, EnvPath, SubstitutionOptions } from "../types.d.ts";
+import { DefaultEnvPath, EnvBase } from "../base/mod.ts";
+import type { Env } from "../types.d.ts";
 
 // deno-lint-ignore no-explicit-any
 const g = globalThis as any;
 const deno = g.Deno;
-
-if (!deno) {
-    throw new Error("deno is not available");
-}
-
-const WIN = deno.build.os === "windows";
-const SEP = WIN ? ";" : ":";
-const PATH_VAR = WIN ? "Path" : "PATH";
-
-/**
- * Represents a class that manages the environment path.
- */
-class DenoEnvPath implements EnvPath {
-    /**
-     * Retrieves the current environment path.
-     * @returns The current environment path.
-     */
-    get(): string {
-        return deno.env.get(PATH_VAR) ?? "";
-    }
-
-    /**
-     * Overwrites the environment path with the specified path.
-     * @param path - The new environment path.
-     */
-    overwrite(path: string): void {
-        deno.env.set(PATH_VAR, path);
-    }
-
-    /**
-     * Splits the environment path into an array of individual paths.
-     * @returns An array of individual paths.
-     */
-    split(): string[] {
-        return this.get().split(SEP);
-    }
-
-    /**
-     * Checks if the environment path contains the specified path.
-     * @param path - The path to check.
-     * @returns `true` if the path is found, `false` otherwise.
-     */
-    has(path: string): boolean {
-        if (WIN) {
-            for (const p of this.split()) {
-                if (path.localeCompare(p, undefined, { sensitivity: "accent" }) === 0) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        return this.split().includes(path);
-    }
-
-    /**
-     * Appends the specified path to the environment path.
-     * @param path - The path to append.
-     */
-    append(path: string): void {
-        const paths = this.split();
-        if (!paths.includes(path)) {
-            paths.push(path);
-            this.overwrite(this.join(paths));
-        }
-    }
-
-    /**
-     * Prepends the specified path to the environment path.
-     * @param path - The path to prepend.
-     */
-    prepend(path: string): void {
-        const paths = this.split();
-        if (!paths.includes(path)) {
-            paths.unshift(path);
-            this.overwrite(this.join(paths));
-        }
-    }
-
-    /**
-     * Removes the specified path from the environment path.
-     * @param path - The path to remove.
-     */
-    remove(path: string): void {
-        const paths = this.split();
-        const index = paths.indexOf(path);
-        if (index !== -1) {
-            paths.splice(index, 1);
-            this.overwrite(this.join(paths));
-        }
-    }
-
-    /**
-     * Replaces the specified old path with the new path in the environment path.
-     * @param oldPath - The path to replace.
-     * @param newPath - The new path.
-     */
-    replace(oldPath: string, newPath: string): void {
-        const paths = this.split();
-        const index = paths.indexOf(oldPath);
-        if (index !== -1) {
-            paths[index] = newPath;
-            this.overwrite(this.join(paths));
-        }
-    }
-
-    /**
-     * Returns an iterator for iterating over the individual paths in the environment path.
-     * @returns An iterator for the individual paths.
-     */
-    *[Symbol.iterator](): IterableIterator<string> {
-        for (const path of this.split()) {
-            yield path;
-        }
-    }
-
-    /**
-     * Returns the string representation of the environment path.
-     * @returns The string representation of the environment path.
-     */
-    toString(): string {
-        return this.get();
-    }
-
-    private join(paths: string[]): string {
-        return paths.join(SEP);
-    }
-}
 
 const proxy = new Proxy({}, {
     get(_target, name) {
@@ -165,45 +36,12 @@ const proxy = new Proxy({}, {
 /**
  * Represents the environment variables and provides methods to interact with them.
  */
-class DenoEnv implements Env {
-    #path?: EnvPath;
+class DenoEnv extends EnvBase {
+    
 
-    /**
-     * Returns a proxy object that allows you to access, set, or delete
-     * environment variables as properties.
-     */
-    get proxy(): Record<string, string | undefined> {
-        return proxy as Record<string, string | undefined>;
-    }
-
-    /**
-     * Returns the `EnvPath` instance associated with this `Env` object.
-     */
-    get path(): EnvPath {
-        return this.#path ??= new DenoEnvPath();
-    }
-
-    /**
-     * Expands a template string by replacing placeholders with their corresponding values.
-     *
-     * @param template The template string to expand.
-     * @param options The options for substitution.
-     * @returns The expanded string.
-     * @example
-     * ```ts
-     * import { env } from "@gnome/env";
-     *
-     * env.set("NAME", "Alice");
-     * console.log(env.expand("Hello, ${NAME}! You are ${AGE:-30} years old.")); // Hello, Alice! You are 30 years old.
-     * ```
-     */
-    expand(template: string, options?: SubstitutionOptions): string {
-        return expand(
-            template,
-            (name: string) => this.get(name),
-            (name: string, value: string) => this.set(name, value),
-            options,
-        );
+    protected override init(): void {
+        super.proxy = proxy;
+        super.path = new DefaultEnvPath(this);
     }
 
     /**
@@ -216,22 +54,7 @@ class DenoEnv implements Env {
         return deno.env.get(name);
     }
 
-    /**
-     * Retrieves the value of the specified environment variable.
-     * If the environment variable is not set, throws an error.
-     *
-     * @param name - The name of the environment variable.
-     * @returns The value of the environment variable.
-     * @throws {Error} If the environment variable is not set.
-     */
-    require(name: string): string {
-        const value = this.get(name);
-        if (value === undefined) {
-            throw new Error(`Environment variable ${name} is not set`);
-        }
-        return value;
-    }
-
+   
     /**
      * Merges the provided environment variables into the current environment.
      * @param env - The environment variables to merge.
@@ -268,7 +91,7 @@ class DenoEnv implements Env {
      * @returns `true` if the environment variable is set, `false` otherwise.
      */
     has(name: string): boolean {
-        return deno.env.get(name) !== undefined;
+        return deno.env.has(name);
     }
 
     *[Symbol.iterator](): IterableIterator<{ key: string; value: string }> {
@@ -310,16 +133,6 @@ class DenoEnv implements Env {
             console.log(`${key}=${values[key]}`);
         }
     }
-
-    /**
-     * Joins an array of paths into a single path string.
-     *
-     * @param paths - An array of paths to be joined.
-     * @returns The joined path string.
-     */
-    joinPath(paths: string[]): string {
-        return paths.join(SEP);
-    }
 }
 
-export const env: DenoEnv = new DenoEnv();
+export const env: Env = new DenoEnv();
--- env/types.d.ts
+++ env/types.d.ts
@@ -112,6 +112,68 @@ export interface Env extends Iterable<{ key: string; value: string }> {
     get path(): EnvPath;
 
     /**
+     * Returns the value associated with the specified name from the environment variables.
+     * If the value is undefined, returns the provided default value.
+     * 
+     * @param name - The name of the environment variable.
+     * @param defaultValue - The default value to return if the environment variable is undefined. Default is an empty string.
+     * @returns The value of the environment variable or the default value if it is undefined.
+     */
+    defaultString(name: string, defaultValue?: string) : string;
+
+    /**
+     * Returns the boolean value associated with the specified name, or the default value if the value is undefined.
+     * 
+     * @param name - The name of the boolean value.
+     * @param defaultValue - The default value to return if the value is undefined. Defaults to `false`.
+     * @returns The boolean value associated with the specified name, or the default value if the value is undefined.
+     */
+    defaultBool(name: string, defaultValue?: boolean) : boolean;
+
+    /**
+     * Returns the value of the specified number configuration option.
+     * If the value is undefined, returns the provided default value.
+     *
+     * @param name - The name of the configuration option.
+     * @param defaultValue - The default value to return if the configuration option is undefined. Default is 0.
+     * @returns The value of the configuration option or the default value.
+     */
+    defaultNumber(name: string, defaultValue?: number) : number
+
+    /**
+     * Retrieves the integer value associated with the specified name from the environment.
+     * If the value is not found, returns the default value.
+     * 
+     * @param name - The name of the environment variable.
+     * @param defaultValue - The default value to return if the environment variable is not found. Default is 0.
+     * @param radix - An optional radix (base) for parsing the integer value. Default is 10.
+     * @returns The integer value associated with the specified name, or the default value if not found.
+     */
+    defaultInt(name: string, defaultValue?: number, radix?: number) : number
+
+    /**
+     * Retrieves the value of the specified date property from the environment.
+     * If the value is undefined, returns the provided defaultValue or a default date value.
+     * @param name - The name of the date property.
+     * @param defaultValue - The default value to return if the property value is undefined. Defaults to a date representing January 1, 1971.
+     * @returns The value of the date property or the default value.
+     */
+    defaultDate(name: string, defaultValue?: Date) : Date
+
+    /**
+     * Retrieves the JSON value with the specified name from the environment and returns it.
+     * If the JSON value is not found, the defaultValue is returned instead.
+     * 
+     * @template T - The type of the JSON value.
+     * @param {string} name - The name of the JSON value.
+     * @param {T} [defaultValue] - The default value to return if the JSON value is not found. 
+     * Defaults to `unknown` and returns an object.
+     * @returns {T} - The JSON value or the defaultValue if not found. Defaults to `unnkown`.
+     */
+    defaultJson<T = unknown>(name: string, defaultValue?: T) : T
+    
+
+    /**
      * Expands a template string by replacing placeholders with their corresponding values.
      *
      * @param template The template string to expand.
@@ -134,6 +196,66 @@ export interface Env extends Iterable<{ key: string; value: string }> {
      * @returns The value of the environment variable, or `undefined` if it is not set.
      */
     get(name: string): string | undefined;
+
+    getArray(name: string, separator?: string) : string[] | undefined;
+
+    getBinary(name: string) : Uint8Array | undefined
+
+     /**
+     * Retrieves a boolean value from the environment variable with the given name.
+     * 
+     * @description
+     * The following values are considered `true`:
+     * - `1`
+     * - `true` (case-insensitive)
+     * 
+     * @param name - The name of the environment variable.
+     * @returns The boolean value of the environment variable, or `undefined` if the variable is not set or has an invalid value.
+     */
+     getBool(name: string) : boolean | undefined;
+
+    /**
+     * Retrieves the value associated with the specified name and converts it to a number.
+     * Returns `undefined` if the value is not found or cannot be converted to a number.
+     * 
+     * @param name - The name of the value to retrieve.
+     * @returns The converted number value, or `undefined` if the value is not found or cannot be converted.
+     * @example
+     * ```ts
+     * import { env } from "@gnome/env";
+     * 
+     * ent.set("PORT", "8080");
+     * console.log(env.getNumber("PORT")); // 8080
+     * ```
+     */
+    getNumber(name: string) : number | undefined;
+
+    /**
+     * Retrieves the integer value associated with the specified name from the environment.
+     * 
+     * @param name - The name of the environment variable.
+     * @param radix - An optional radix (base) for parsing the value. Defaults to 10 if not provided.
+     * @returns The parsed integer value, or `undefined` if the value is not found or cannot be parsed as an integer.
+     */
+    getInt(name: string, radix?: number) : number | undefined;
+
+    /**
+     * Retrieves the value associated with the specified name and converts it to a Date object.
+     * 
+     * @param name - The name of the value to retrieve.
+     * @returns The Date object representing the value, or undefined if the value is not found.
+     */
+    getDate(name: string) : Date | undefined;
+
+    /**
+     * Retrieves a JSON value from the environment variables by name.
+     * 
+     * @param name - The name of the environment variable.
+     * @returns The parsed JSON value if it exists, otherwise undefined.
+     * @template T - The type of the parsed JSON value. Defaults to `unknown`.
+     */
+    getJson<T = unknown>(name: string) : T | undefined
+
 
     /**
      * Retrieves the value of the specified environment variable.
